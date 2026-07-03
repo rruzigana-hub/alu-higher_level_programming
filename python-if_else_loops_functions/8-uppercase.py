80b43cbbbcc0d9979ee4edbfe1829639074f0170
rruzigana-hub <r.ruzigana@alustudent.com>
2026-07-03 03:21:00 -0700

    8-uppercase: fix pycodestyle indentation issue

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -5,6 +5,6 @@
 def uppercase(str):
     """Print str in uppercase, followed by a newline."""
     for c in str:
-        print("{:c}".format(ord(c) - 32 if 97 <= ord(c) <= 122
-                             else ord(c)), end="")
+        n = ord(c) - 32 if 97 <= ord(c) <= 122 else ord(c)
+        print("{:c}".format(n), end="")
     print()
