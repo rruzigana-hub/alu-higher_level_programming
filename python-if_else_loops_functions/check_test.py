2ab63a492ddf3c56b51ebd5dfe8bf20bdde04321
rruzigana-hub <r.ruzigana@alustudent.com>
2026-07-03 04:14:44 -0700

    Remove debug test file

--- python-if_else_loops_functions/check_test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("holberton")
-uppercase("Holberton School")
-uppercase("Holberton School, 98 battery street")
-uppercase("")
-uppercase("98")
-uppercase("z")
