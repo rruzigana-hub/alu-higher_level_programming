2744150fd10dd6a3f75c72873d506b1fc5af780f
rruzigana-hub <r.ruzigana@alustudent.com>
2026-07-04 15:07:30 -0700

    2-args: print number and list of command line arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Program that prints the number and list of its arguments."""
+from sys import argv
+
+if __name__ == "__main__":
+    n = len(argv) - 1
+    if n == 0:
+        print("0 arguments.")
+    elif n == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(n))
+
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
