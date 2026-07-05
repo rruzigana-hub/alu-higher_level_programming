23eb5031d57a6194054b2b4ac2f8299479097817
rruzigana-hub <r.ruzigana@alustudent.com>
2026-07-05 03:25:40 -0700

    7-update_dictionary: add or update key-value pair in dictionary

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that adds or updates a key/value pair in a dictionary."""
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Set a_dictionary[key] to value and return the dictionary."""
+    a_dictionary[key] = value
+    return a_dictionary
