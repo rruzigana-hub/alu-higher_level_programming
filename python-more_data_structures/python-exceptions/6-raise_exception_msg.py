e90d5cfb3ca36b999806ae19f5d95f5f02cf8349
rruzigana-hub <r.ruzigana@alustudent.com>
2026-07-05 03:53:42 -0700

    6-raise_exception_msg: raise NameError with custom message

---
+++ python-more_data_structures/python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that raises a NameError with a custom message."""
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError with the given message."""
+    raise NameError(message)
