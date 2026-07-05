9244c3c929e113cb8a1981792bbcd7715f4a15b7
rruzigana-hub <r.ruzigana@alustudent.com>
2026-07-05 03:15:35 -0700

    3-common_elements: find common elements between two sets

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that finds common elements between two sets."""
+
+
+def common_elements(set_1, set_2):
+    """Return a set of elements present in both set_1 and set_2."""
+    return set_1 & set_2
