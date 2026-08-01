660391e9cdfa877ee5e5b0907ada8dc113597177
rruzigana-hub <r.ruzigana@alustudent.com>
2026-08-01 02:54:21 -0700

    Fix 4-header.sh: send X-School-User-Id header to match real checker

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with a custom header and displays the body
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request with the required custom header and displays the body
+curl -s -H "X-School-User-Id: 98" -H "X-HolbertonSchool-User-Id: 98" "$1"
